f15371a6415ca8186d5d0b4e49a45f64805a66ea
Ngurah Prihandana <prihandana27@gmail.com>
2020-04-07 07:59:52 +0800

    Memperbaiki bug list pada menu user

--- app/views/user/available.php
+++ app/views/user/available.php
@@ -1,29 +1,29 @@
 <div class="container">
 
+<div class="columns">
+
 <div class="columns mt-1">
 <?php foreach ($items as $item) { ?>
-
-<div class="columns is-one-quarter">
-<div class="card" >
-  <header class="card-header">
-    <p class="card-header-title">
-      <?= $item["nama"] ?>
-    </p>
-  </header>
-  <div class="card-content">
-    <div class="content">
-        <?= $item["deskripsi"] ?>
+  <div class="card column" >
+    <header class="card-header">
+      <p class="card-header-title">
+        <?= $item["nama"] ?>
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+          <?= $item["deskripsi"] ?>
+      </div>
     </div>
+    <footer class="card-footer">
+      <p class="card-footer-item">Tersedia: <?= $item["available_qty"] ?></p>
+      <a href="#" class="card-footer-item">Pinjam</a>
+    </footer>
   </div>
-  <footer class="card-footer">
-    <p class="card-footer-item">Tersedia: <?= $item["available_qty"] ?></p>
-    <a href="#" class="card-footer-item">Pinjam</a>
-  </footer>
-</div>
-</div>
-
-
 <?php } ?>
+</div>
+
+
 
 </div>
 </div>
