e29f5c815c3362a9e490d1d26f0c32370c337c14
Mr. R <60025301+RamPramartha@users.noreply.github.com>
2020-04-08 08:50:33 +0800

    Menambahkan Tabel Kepala Jurusan

---
+++ app/controllers/KepalaJurusan.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kepala Jurusan</title>
+</head>
+<body>
+    <center>
+        <h1>Kepala Jurusan</h1>
+        <table width="80%"border="1" bordercolor="black"cellpadding="10px" cellspacing="0px">
+            <tr>
+                <th>Id Jurusan</th>
+                <th>Nama</th>
+                <th>Id Kepala Jurusan</th>
+                <th>Kode Jurusan</th>
+                <th>Deskripsi</th>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
